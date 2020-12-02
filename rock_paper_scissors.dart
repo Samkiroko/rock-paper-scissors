f37b47975956adb672b384355d8032e845bfd3e8
Samkiroko <kirokodev@gmail.com>
2020-12-02 14:05:33 +0300

    setting the pseudocode for game logic

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -1,3 +1,13 @@
 void main() {
   print('hello world');
+  // show prompt
+  // Read user from console
+  // if input is valid move("r","p","s")
+  // choose the AI move at random
+  // compare the player move with the AI move
+  // Show the result
+  // else if input is "q"
+  // quit the program
+  // else
+  // Invalid input
 }
